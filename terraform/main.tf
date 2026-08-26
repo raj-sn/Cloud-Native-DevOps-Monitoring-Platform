e973d7af292d36b9d0799654b9319a6590d1b53c
raj-sn <usnr661@gmail.com>
2026-08-27 01:33:00 +0530

    Add initial Terraform configuration

---
+++ terraform/main.tf
@@ -0,0 +1,56 @@
+resource "aws_security_group" "web_sg" {
+
+  name = "employee-platform-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web_server" {
+
+  ami           = "ami-0b6d9d3d33ba97d99"
+
+  instance_type = var.instance_type
+
+  key_name = "terraform-key"
+
+  vpc_security_group_ids = [
+    aws_security_group.web_sg.id
+  ]
+
+  tags = {
+    Name = "Employee-Platform-Server"
+  }
+}
